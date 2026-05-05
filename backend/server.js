db58b5dbd94030b25c1d106ffd99d2d814852c67
Hamza-rgb-hub <hb1967914@gmail.com>
2026-05-05 16:13:20 +0500

    update middleware

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,20 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_URL , credentials: true }));
+const allowedOrigins = "https://ecommerce-fullstack-six-orpin.vercel.app"
+;
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
